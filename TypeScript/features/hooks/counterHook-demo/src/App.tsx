348f8c7b074e8e06385bebd749019588af951512
Elliot <elliotgjy@163.com>
2024-02-26 16:17:55 +0800

    update

---
+++ TypeScript/features/hooks/counterHook-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Issues2 from './Issues'
+import Counter from './Counter';
+
+
+const App = () => {
+  return (
+    <Counter />
+    // <Issues2 />
+  )
+}
+
+export default App;
+
---
+++ TypeScript/features/router/router-demo/src/routes.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+
+import App from './App'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Error404 from './components/Error404'
+
+const AppRoutes = () => (
+  <App>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  </App>
+)
+
+export default AppRoutes
